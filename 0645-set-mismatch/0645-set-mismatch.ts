9cdb797c4dd88804d0a51340694d70769599d3f5
Vladislav Latanov <87940833+Paris22@users.noreply.github.com>
2023-06-26 12:51:33 +0300

    Time: 71 ms (82.05%), Space: 44.9 MB (100%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.ts
@@ -0,0 +1,8 @@
+const findErrorNums = (nums: number[]): number[] => {
+    const arr: number[] = new Array(nums.length+1).fill(0);
+    for(let i = 0; i < nums.length; i++) {
+        arr[nums[i]]++;
+    }
+
+    return [arr.indexOf(2), arr.lastIndexOf(0)];
+};
